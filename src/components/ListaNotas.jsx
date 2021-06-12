f43893a39194086932e160be007f04e6e892d1dd
Adriano Maurmann <adriano.maurmann@ecs.com.br>
2021-06-11 23:23:45 -0300

    adicionado bloco js em listanotas.jsx

--- src/components/ListaNotas.jsx
+++ src/components/ListaNotas.jsx
@@ -1,16 +1,21 @@
-import React, { Component } from 'react';
-import CardNota from './CardNota';
+import React, { Component } from "react";
+import CardNota from "./CardNota";
 
 class ListaNotas extends Component {
-    render() { 
-        return ( 
-            <ul>
-                <li>
-                    <CardNota></CardNota>
-                </li>
-            </ul>
-         );
-    }
+  render() {
+    return (
+      <ul>
+        {Array.of("Trabalho", "Trabalho", "Estudo").map((categoria) => {
+          return (
+            <li>
+              <div>{categoria}</div>
+              <CardNota></CardNota>
+            </li>
+          );
+        })}
+      </ul>
+    );
+  }
 }
- 
+
 export default ListaNotas;
